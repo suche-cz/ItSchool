3517fa429ecc5853f621a24288e21c0b7635a8ca
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2025-03-06 17:54:19 +0100

    add main style

--- _project/urls.py
+++ _project/urls.py
@@ -20,3 +20,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# from django.conf import settings
+# from django.conf.urls.static import static
+# static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
