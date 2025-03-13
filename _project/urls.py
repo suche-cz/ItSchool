4aab36a874744c7e065fdf7d7570d32f1e3acab6
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2025-03-13 18:56:59 +0100

    add debug tool only on debug

--- _project/urls.py
+++ _project/urls.py
@@ -18,9 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from debug_toolbar.toolbar import debug_toolbar_urls
+
 
 urlpatterns = [
     path('', include('school.urls', namespace='school')),
     path('admin/', admin.site.urls),
-] + debug_toolbar_urls() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+    urlpatterns += debug_toolbar_urls()
